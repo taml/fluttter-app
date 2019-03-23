01affc9fa10a7b5c394c88e75368b9c83fc9bf3b
Tam Love <dark_angel4eva@email.com>
2019-03-23 19:48:50 +0000

    Added AppBar Tabs.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
+const fluttterDarkGrey = const Color(0xFF383838);
+
 void main() => runApp(FluttterApp());
 
 class FluttterApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,46 +15,67 @@ class FluttterApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Color(0xFF383838),
-        brightness: Brightness.dark,
-        leading: IconButton(
-          icon: Icon(
-            Icons.menu,
-            color: Colors.grey[300],
-          ),
-          onPressed: null,
-        ),
-        title: Text(
-          'Fluttter',
-          style: TextStyle(
-            fontFamily: 'Pacifico',
-            fontSize: 26.0,
-          ),
-        ),
-        actions: <Widget>[
-          IconButton(
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: fluttterDarkGrey,
+          brightness: Brightness.dark,
+          leading: IconButton(
             icon: Icon(
-              Icons.show_chart,
+              Icons.menu,
               color: Colors.grey[300],
-              semanticLabel: 'activity',
             ),
             onPressed: null,
           ),
-          IconButton(
-            icon: Icon(
-              Icons.search,
-              color: Colors.grey[300],
-              semanticLabel: 'search',
+          title: Text(
+            'Fluttter',
+            style: TextStyle(
+              fontFamily: 'Pacifico',
+              fontSize: 26.0,
             ),
-            onPressed: null,
           ),
-        ],
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(
+                Icons.show_chart,
+                color: Colors.grey[300],
+                semanticLabel: 'activity',
+              ),
+              onPressed: null,
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.grey[300],
+                semanticLabel: 'search',
+              ),
+              onPressed: null,
+            ),
+          ],
+          bottom: TabBar(
+            indicatorColor: Color(0xFFEA4C89),
+            unselectedLabelColor: Colors.grey[300],
+            labelColor: Colors.white,
+            labelStyle: TextStyle(
+                fontSize: 16.0,
+            ),
+            tabs: [
+              Tab(
+                text: 'FOLLOWING',
+              ),
+              Tab(
+                text: 'POPULAR',
+              ),
+              Tab(
+                text: 'RECENT',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
