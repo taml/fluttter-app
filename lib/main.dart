d526c0c94ddebffde8e4252786d877d9b8c63e50
Tam Love <dark_angel4eva@email.com>
2019-03-27 16:42:01 +0000

    Added Post footer.

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,6 @@ class FluttterApp extends StatelessWidget {
 }
 
 class HomePage extends StatelessWidget {
-
   Widget _buildTabs() {
     return TabBar(
       indicatorColor: fluttterPink,
@@ -98,31 +97,32 @@ class HomePage extends StatelessWidget {
 
 class BodyContent extends StatelessWidget {
 
-  var _postHeader = Row(
-    mainAxisAlignment: MainAxisAlignment.start,
-    crossAxisAlignment: CrossAxisAlignment.start,
-    children: <Widget>[
-      Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: CircleAvatar(
-              backgroundImage: AssetImage('assets/dog1.png'),
-              radius: 22.0,
-            ),
-          ),
-        ],
-      ),
-      Expanded(
-        child: Column(
+  Widget _buildPostHeader() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Column(
           mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            Row(
-              children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(4.0, 20.0, 16.0, 0.0),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: CircleAvatar(
+                backgroundImage: AssetImage('assets/dog1.png'),
+                radius: 22.0,
+              ),
+            ),
+          ],
+        ),
+        Expanded(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(4.0, 18.0, 16.0, 0.0),
                     child: Text(
                       'Love is Color',
                       textAlign: TextAlign.start,
@@ -134,64 +134,171 @@ class BodyContent extends StatelessWidget {
                       ),
                     ),
                   ),
-              ],
-            ),
-            Row(
-              children: <Widget>[
-                Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(4.0, 0.0, 2.0, 16.0),
-                      child: Text(
-                        'Fluffy McScruffins,',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontFamily: fluttterMainFont,
-                          fontWeight: FontWeight.w700,
-                          color: fluttterBlue,
-                          fontSize: 15.0,
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(4.0, 0.0, 2.0, 16.0),
+                        child: Text(
+                          'Fluffy McScruffins,',
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontFamily: fluttterMainFont,
+                            fontWeight: FontWeight.w700,
+                            color: fluttterBlue,
+                            fontSize: 15.0,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-                Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0.0, 0.0, 16.0, 16.0),
-                      child: Text(
-                        '3 days ago',
-                        textAlign: TextAlign.start,
-                        style: TextStyle(
-                          fontFamily: fluttterMainFont,
-                          fontWeight: FontWeight.w600,
-                          color: fluttterLightGrey,
-                          fontSize: 15.0,
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Container(
+                        padding: const EdgeInsets.fromLTRB(0.0, 0.0, 16.0, 16.0),
+                        child: Text(
+                          '3 days ago',
+                          textAlign: TextAlign.start,
+                          style: TextStyle(
+                            fontFamily: fluttterMainFont,
+                            fontWeight: FontWeight.w600,
+                            color: fluttterLightGrey,
+                            fontSize: 15.0,
+                          ),
                         ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-          ],
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
-      ),
-    ],
-  );
+      ],
+    );
+  }
 
   Widget _buildPostBody(BuildContext context) {
     return Row(
       children: <Widget>[
         Row(
           children: <Widget>[
-            Image.asset('assets/1.jpg', width: MediaQuery
-                .of(context)
-                .size
-                .width, fit: BoxFit.cover),
+            Image.asset('assets/1.jpg',
+                width: MediaQuery.of(context).size.width, fit: BoxFit.cover),
           ],
         ),
       ],
+    );
+  }
+
+  Widget _buildPostFooter() {
+    return Container(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.favorite,
+                      color: Colors.grey,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(8.0, 2.0, 8.0, 0.0),
+                      child: Text(
+                        '30',
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontFamily: fluttterMainFont,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.chat_bubble,
+                      color: Colors.grey,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(8.0, 2.0, 8.0, 0.0),
+                      child: Text(
+                        '0',
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontFamily: fluttterMainFont,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                    Icon(
+                      Icons.visibility,
+                      color: Colors.grey,
+                    ),
+                    Padding(
+                      padding: EdgeInsets.fromLTRB(8.0, 2.0, 8.0, 0.0),
+                      child: Text(
+                        '177',
+                        style: TextStyle(
+                          color: Colors.grey[700],
+                          fontFamily: fluttterMainFont,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 16.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Icon(
+                    Icons.reply,
+                    color: Colors.grey,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(8.0, 2.0, 8.0, 0.0),
+                    child: Text(
+                      '1',
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                        fontFamily: fluttterMainFont,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  Icon(
+                    Icons.attach_file,
+                    color: Colors.grey,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(8.0, 2.0, 8.0, 0.0),
+                    child: Text(
+                      '3',
+                      style: TextStyle(
+                        color: Colors.grey[700],
+                        fontFamily: fluttterMainFont,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 
@@ -204,8 +311,9 @@ class BodyContent extends StatelessWidget {
       ),
       child: Column(
         children: <Widget>[
-          _postHeader,
+          _buildPostHeader(),
           _buildPostBody(context),
+          _buildPostFooter(),
         ],
       ),
     );
@@ -213,19 +321,17 @@ class BodyContent extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     List<Card> testList = [_buildPostCard(context)];
 
     return TabBarView(
       children: <Widget>[
         ListView.builder(
           itemCount: 1,
-            itemBuilder: (BuildContext context, int index) => testList[index],
+          itemBuilder: (BuildContext context, int index) => testList[index],
         ),
         Text('2'),
         Text('3'),
       ],
     );
   }
-
 }
--- lib/models/post.dart
+++ lib/models/post.dart
@@ -2,9 +2,15 @@ class Post {
   int likes;
   int comments;
   int views;
+  int refluttts;
+  int attachments;
 
   Post(this.title, this.author, this.authorImg, this.date, this.postImg,
-      [this.likes = 0, this.comments = 0, this.views = 0])
+      [this.likes = 0,
+      this.comments = 0,
+      this.views = 0,
+      this.refluttts = 0,
+      this.attachments = 0])
       : assert(title != null),
         assert(author != null),
         assert(authorImg != null),
@@ -16,5 +22,4 @@ class Post {
   final String authorImg;
   final int date;
   final String postImg;
-
 }
